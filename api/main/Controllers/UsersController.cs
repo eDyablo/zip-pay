a075a813604fb3393c7c0224b9c37671fdd40535
edyablo <eyablonsky@gmail.com>
2020-07-11 17:49:11 +0300

    user basics

---
+++ api/main/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace api
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class UsersController : ControllerBase {
+    Database database;
+
+    public UsersController(Database database) {
+      this.database = database;
+    }
+
+    [HttpGet]
+    public string Get() {
+      return database.Version;
+    }
+
+    [HttpPost]
+    public string Post([FromBody]UserRecord record) {
+      var answer = new StringBuilder();
+      answer.AppendLine($"name:             {record.Name}");
+      answer.AppendLine($"mail:             {record.Mail}");
+      answer.AppendLine($"monthly salary:   {record.Salary}");
+      answer.AppendLine($"monthly expenses: {record.Expenses}");
+      return answer.ToString();
+    }
+  }
+}
---
+++ api/main/Models/UserRecord.cs
@@ -0,0 +1,13 @@
+namespace api.Models {
+  public class UserRecord {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Mail { get; set; }
+
+    public float Salary { get; set; }
+
+    public float Expenses { get; set; }
+  }
+}
