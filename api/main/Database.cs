6cf9b6aaf68511d9cf1168904e4ea06385f7a985
edyablo <eyablonsky@gmail.com>
2020-07-11 17:01:47 +0300

    create tables

--- api/main/Database.cs
+++ api/main/Database.cs
@@ -1,6 +1,5 @@
 using Npgsql;
 using System;
-using System.Data.Common;
 
 namespace api
 {
@@ -11,11 +10,35 @@ namespace api
     public Database(string connectionString) {
       connection = new NpgsqlConnection(connectionString);
       connection.Open();
-      Version = new NpgsqlCommand("SELECT version()", connection).ExecuteScalar().ToString();
+      Version = ExecuteScalar("SELECT version()").ToString();
+
+      ExecuteNonQuery(@"
+        |CREATE TABLE IF NOT EXISTS users(
+        | id SERIAL PRIMARY KEY,
+        | name VARCHAR(255),
+        | email VARCHAR(255),
+        | salary real,
+        | expenses real
+        |)".StripMargin());
+
+      ExecuteNonQuery(@"
+        |CREATE TABLE IF NOT EXISTS accounts(
+        | id SERIAL PRIMARY KEY,
+        | user_id INTEGER REFERENCES users(id),
+        | name VARCHAR(255)
+        |)".StripMargin());
     }
 
     public void Dispose() {
       connection.Close();
+    }
+
+    private object ExecuteScalar(string command) {
+      return new NpgsqlCommand(command, connection).ExecuteScalar();
+    }
+
+    private void ExecuteNonQuery(string command) {
+      new NpgsqlCommand(command, connection).ExecuteNonQuery();
     }
   }
 }
